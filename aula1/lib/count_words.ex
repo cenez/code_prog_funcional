ec185e9bf515c41d8be46352212f99c928560563
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2024-10-20 22:29:58 -0300

    ufc

--- aula1/lib/count_words.ex
+++ aula1/lib/count_words.ex
@@ -19,7 +19,7 @@ defmodule CountWords do
 
   def start(_type, _args) do
     IO.puts "Iniciando..."
-    file_name = "/mnt/c/workspace/elixir/words.txt"
+    file_name = "/home/cenez/workspace/elixir/code_prog_funcional/words.txt"
     file_name |> count() |> IO.inspect
     {:ok, self()}
   end
--- aula2/lib/aula2.ex
+++ aula2/lib/aula2.ex
@@ -1,7 +1,7 @@
 defmodule Aula2 do
   def start(_type, _args) do
     IO.puts "Iniciando..."
-    file_name = "/home/cenez/workspace/ufc/pf/elixir/words.txt"
+    file_name = "/home/cenez/workspace/elixir/code_prog_funcional/words.txt"
     file_name |> CountWords.count() |> IO.inspect
     {:ok, self()}
   end
